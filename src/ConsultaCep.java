903085e952290505cf72ad72ffe6ada2cd1aa362
Michel Marinho <marinhomich@gmail.com>
2024-04-22 15:36:05 -0400

    update files

---
+++ src/ConsultaCep.java
@@ -0,0 +1,24 @@
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import com.google.gson.Gson;
+
+public class ConsultaCep {
+
+    public Endereco buscaEndereco(String cep) {
+        URI endereco = URI.create("https://viacep.com.br/ws/" + cep + "/json");
+
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder().uri(endereco).build();
+
+        try {
+            HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+            return new Gson().fromJson(response.body(), Endereco.class);
+        } catch (Exception e) {
+            throw new RuntimeException("Não consegui obter o endereço a partir desse CEP");
+        }
+
+
+    }
+}
---
+++ src/Endereco.java
@@ -0,0 +1,3 @@
+public record Endereco(String logradouro, String cep, String localidade, String bairro, String uf, String complemento) {
+
+}
